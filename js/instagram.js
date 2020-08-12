41b189ae6c1d16e056313002a6720dea87afba17
Sayem Chowdhury <sayem314@gmail.com>
2020-08-13 02:14:02 +0600

    support albums

--- js/instagram.js
+++ js/instagram.js
@@ -1,24 +1,27 @@
 const instagram = (data) => {
   const media = data.graphql.shortcode_media;
   data.title = media.shortcode;
+  data.is_album = true;
 
-  if (media.is_video) {
-    data.formats = [
-      {
-        protocol: "https",
-        format: `0 - ${media.dimensions.height}x${media.dimensions.width}`,
-        url: media.video_url,
-        ext: "mp4",
-        format_id: 0,
-      },
-    ];
+  if (media.edge_sidecar_to_children) {
+    data.formats = media.edge_sidecar_to_children.edges.map(
+      ({ node: item }, index) => {
+        return {
+          protocol: "https",
+          format: `${index} - ${item.dimensions.height}x${item.dimensions.width}`,
+          url: item.is_video ? item.video_url : item.display_url,
+          ext: item.is_video ? "mp4" : "jpg",
+          format_id: index,
+        };
+      }
+    );
   } else {
     data.formats = [
       {
         protocol: "https",
         format: `0 - ${media.dimensions.height}x${media.dimensions.width}`,
-        url: media.display_url,
-        ext: "jpg",
+        url: media.is_video ? media.video_url : media.display_url,
+        ext: media.is_video ? "mp4" : "jpg",
         format_id: 0,
       },
     ];
