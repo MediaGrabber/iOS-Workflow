99c900814c95a9832ebbc06eb3a26668a69f38f6
Sayem Chowdhury <sayem314@gmail.com>
2020-09-10 10:31:39 +0600

    use webpack

---
+++ bundle.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+yarn webpack js/index.js -o dist/bundle.js --mode=production
+BUNDLE="dist/bundle.js"
+JS=$(cat "$BUNDLE")
+HTML="<!DOCTYPE html>
+  <html>
+    <body>
+      <p id="content">data</p>
+
+      <script>
+      $JS
+     </script>
+    </body>
+  </html>"
+echo "$HTML" > "$BUNDLE"
+
+cat $BUNDLE | curl -F "clbin=<-" http://sprunge.us/
---
+++ js/youtube/bundle.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+yarn webpack js/youtube/index.js -o dist/bundle.js --mode=production
+BUNDLE="dist/bundle.js"
+JS=$(cat "$BUNDLE")
+HTML="<!DOCTYPE html>
+  <html>
+    <body>
+      <p id="content">data</p>
+      <p id="player">data</p>
+      <p id="url">data</p>
+
+      <script>
+      $JS
+     </script>
+    </body>
+  </html>"
+echo "$HTML" > "$BUNDLE"
+
+cat $BUNDLE | curl -F "clbin=<-" http://sprunge.us/
